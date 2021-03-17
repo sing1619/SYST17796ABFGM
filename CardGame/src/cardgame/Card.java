adefec78a4fa35b0da23a87febd65ff11d89e4e3
gurus <gurus@192.168.2.13>
2021-03-17 16:58:30 -0400

    After refactoring using enums (Loosely coupled)

--- CardGame/src/cardgame/Card.java
+++ CardGame/src/cardgame/Card.java
@@ -2,39 +2,44 @@ package cardgame;
 
 public class Card {
 
-	private int value;
-	private String suit;
-        
-        public static final String[] SUITS =
-        {"Hearts","Diamonds","Spades","Clibs"};
+    public enum Suit {
+        HEARTS, SPADES, CLUBS, DIAMONDS
+    }
 
-    public Card(int value, String suit) {
+    public enum Value {
+        ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING
+    }
+
+    private Value value;
+    private Suit suit;
+
+    public Card(Value value, Suit suit) {
         this.value = value;
         this.suit = suit;
     }
-        
-	public int getValue() {
-		return this.value;
-	}
 
-	/**
-	 * 
-	 * @param value
-	 */
-	public void setValue(int value) {
-		this.value = value;
-	}
+    public Value getValue() {
+        return this.value;
+    }
 
-	public String getSuit() {
-		return this.suit;
-	}
+    /**
+     *
+     * @param value
+     */
+    public void setValue(Value value) {
+        this.value = value;
+    }
 
-	/**
-	 * 
-	 * @param suit
-	 */
-	public void setSuit(String suit) {
-		this.suit = suit;
-	}
+    public Suit getSuit() {
+        return this.suit;
+    }
+
+    /**
+     *
+     * @param suit
+     */
+    public void setSuit(Suit suit) {
+        this.suit = suit;
+    }
 
 }
--- CardGame/src/cardgame/CardGame.java
+++ CardGame/src/cardgame/CardGame.java
@@ -1,5 +1,7 @@
 package cardgame;
 
+import cardgame.Card.Suit;
+import cardgame.Card.Value;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -13,16 +15,22 @@ public class CardGame {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+
         Scanner input = new Scanner(System.in);
 
         //create an array to hold 7 cards
         Card[] hand = new Card[7];
 
+        Value[] cardValues = Value.values();
+
+        Suit[] cardSuits = Suit.values();
+
         //we will use Random to generate random numbers
         Random random = new Random();
+
         for (int i = 0; i < hand.length; i++) {
-            int value = random.nextInt(13) + 1;
-            String suit = Card.SUITS[random.nextInt(4)];
+            Value value = cardValues[random.nextInt(cardValues.length)];
+            Suit suit = cardSuits[random.nextInt(cardSuits.length)];
 
             Card card = new Card(value, suit);
             hand[i] = card;
@@ -30,21 +38,27 @@ public class CardGame {
         // print them out debugging purposes
         System.out.println("Here are the cards in the hand");
         for (Card card : hand) {
-            System.out.printf("%d of %s\n", card.getValue(), card.getSuit());
+            System.out.printf("%s of %s\n", card.getValue(), card.getSuit());
         }
         //Now ask the user for a card      
         System.out.println("Pick a suit for your card");
-        for (int i = 0; i < Card.SUITS.length; i++) {
-            System.out.println((i + 1) + ":" + Card.SUITS[i]);
+        for (int i = 0; i < cardSuits.length; i++) {
+            System.out.println((i + 1) + ":" + cardSuits[i]);
         }
-        
+
         //Ask user to enter card value from 1 to 4
-        int suit = input.nextInt();
-        
+        int suitPosition = input.nextInt() - 1;
+
         //Ask user to enter card number
-        System.out.println("Enter a value (1 to 13)");
-        int value = input.nextInt();
-        Card userGuess = new Card(value, Card.SUITS[suit - 1]);
+        System.out.println("Enter a value ");
+        for (int i = 0; i < cardValues.length; i++) {
+            System.out.println((i + 1) + ":" + cardValues[i]);
+        }
+
+        int valuePosition = input.nextInt() - 1;
+
+        Card userGuess = new Card(cardValues[valuePosition], cardSuits[suitPosition]);
+
         boolean match = false;
         for (Card card : hand) {
             if (card.getValue() == userGuess.getValue()
